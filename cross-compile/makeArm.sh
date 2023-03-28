a727274e176e8d7fc97d84789949bcc3f6dfc70b
Your Name <you@example.com>
2023-03-27 19:19:27 -0700

    Updating pre-compiled binaries

--- cross-compile/makeArm.sh
+++ cross-compile/makeArm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make linux STATIC=-static CROSS_COMPILE=/home/user/buildroot-2015.05-armv7l/output/host/usr/bin/arm-linux-
+make linux STATIC=-static CROSS_COMPILE=/home/user/buildroot-2015.05-armv7l-musl/output/host/usr/bin/arm-linux-
